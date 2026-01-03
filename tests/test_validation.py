9d5c9e194508a4de07862d08e041b44c265e14b6
Ken Chan <lkc562@gmail.com>
2026-01-03 00:17:01 -0500

    docstrings for test_validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,5 +1,54 @@
 import pandas as pd
 import yaml
 import pytest
-from data.validation import validate_schema
+from src.data.validation import validate_schema
 
+def test_schema_valid_data():
+    """
+    Tests a df with a valid schema to the yaml schema.
+
+    Params:
+        None
+
+    Returns:
+        None
+    """
+    df = pd.DataFrame({
+        "age":[25],
+        "sex":["female"],
+        "bmi":[22.5],
+        "children":[1],
+        "smoker":["yes"],
+        "region":["southwest"],
+        "charges":[5000]
+    })
+
+    with open("config/schema.yaml",encoding="utf-8") as f:
+        schema = yaml.safe_load(f)
+
+    validate_schema(df, schema) # should not raise
+
+def test_schema_missing_column():
+    """
+    Tests a df with a missing column to the yaml schema.
+
+    Params:
+        None
+
+    Returns:
+        None
+    """
+    df = pd.DataFrame({
+        "age":[25],
+        "sex":["female"],
+        "bmi":[22.5],
+        "children":[1],
+        "smoker":["yes"],
+        "charges":[5000]
+    })
+
+    with open("config/schema.yaml",encoding="utf-8") as f:
+        schema = yaml.safe_load(f)
+
+    with pytest.raises(Exception):
+        validate_schema(df,schema)

d8bf9ccf34f858c91820b0ee5100a0f7e54d2583
lokkenchan <lkc562@gmail.com>
2026-01-22 11:52:04 -0500

    comments on how path.py works

--- linear_regression_001/utils/paths.py
+++ linear_regression_001/utils/paths.py
@@ -1,5 +1,10 @@
 from pathlib import Path
 
+# __file__ gets you this file path
+# Path(__file__) returns pathlib.Path object to perform operations
+# resolve creates the absolute path
+# parents[0] is utils package (b/c parent of file), parents[1] is linear_regression_001
+# parents[2] is linear_regression_001 as a root linear_regression_001/linear_regression_001 (the first)
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 DATA_DIR = PROJECT_ROOT / "data"

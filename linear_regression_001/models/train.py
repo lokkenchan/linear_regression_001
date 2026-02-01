de7763bc26e6056bb4184717e5b5708c816bf48c
lokkenchan <lkc562@gmail.com>
2026-02-01 11:17:33 -0500

    Fix train py by adding Pipeline and ColumnTransformer to approach text based data

--- linear_regression_001/models/train.py
+++ linear_regression_001/models/train.py
@@ -1,5 +1,8 @@
 import joblib
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LinearRegression
 from sklearn.dummy import DummyRegressor
 
@@ -13,14 +16,31 @@ def train_models():
     X, y = split_features_target(df)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+    # Dectect column types
+    categorical_cols = X_train.select_dtypes(exclude="number").columns
+    numeric_cols = X_train.select_dtypes(include="number").columns
+
+    # Preprocessing
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols),
+            ("num", "passthrough", numeric_cols),
+        ]
+    )
+
+    model = Pipeline(steps=[
+        ("preprocessing", preprocessor),
+        ("regressor", LinearRegression())
+    ])
+
+    # Train linear regression model
+    model.fit(X_train, y_train)
+
     # Train baseline dummy model
     baseline = DummyRegressor(strategy = "mean")
     baseline.fit(X_train,y_train)
 
-    # Train linear regression model
-    model = LinearRegression()
-    model.fit(X_train,y_train)
-
+    # Save
     joblib.dump(model, MODELS / "linear_regression.pkl")
     joblib.dump(baseline, MODELS / "baseline.pkl")
 

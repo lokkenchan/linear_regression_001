fe42aacb345b632a6f91ba233b439fbb58b04bee
lokkenchan <lkc562@gmail.com>
2026-02-02 17:19:16 -0500

    Remove features directory path and add documentation for remaining paths

--- linear_regression_001/utils/paths.py
+++ linear_regression_001/utils/paths.py
@@ -7,12 +7,19 @@ from pathlib import Path
 # parents[2] is linear_regression_001 as a root linear_regression_001/linear_regression_001 (the first)
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
+# Data directory
 DATA_DIR = PROJECT_ROOT / "data"
+# Uneditted data
 RAW = DATA_DIR / "raw"
-INTERIM = DATA_DIR / "interim"
+# External supplementary data to be combined with raw data
 EXTERNAL = DATA_DIR / "external"
+# Cleaned data
+INTERIM = DATA_DIR / "interim"
+# Data that includes feature engineered columns - excluding transformations that require a pipeline to prevent data-leakage
 PROCESSED = DATA_DIR / "processed"
-FEATURES = DATA_DIR / "features"
+# Data that includes just features to predict
 INFERENCE = DATA_DIR / "inference"
+# Output of Inference data
 PREDICTIONS = DATA_DIR/ "predictions"
+# Where models are saved
 MODELS = PROJECT_ROOT / "linear_regression_001/models/saved_models/"

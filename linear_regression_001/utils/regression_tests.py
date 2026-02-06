d8ffbec25d10e4be1bcf09b486260787b61a4eb7
lokkenchan <lkc562@gmail.com>
2026-02-06 18:49:09 -0500

    linear regression assumption notes for feature_engineering and modeling.ipynb and regression_tests.py started within utils

---
+++ linear_regression_001/utils/regression_tests.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+# statsmodels
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor, OLSInfluence
+from statsmodels.stats.diagnostic import het_breuschpagan
+from statsmodels.stats.stattools import durbin_watson
+from statsmodels.regression.linear_model import OLS
+
+def calculate_vif():
+    pass
+
+def plot_feature_correlations():
+    pass
+
+def plot_linearity_checks():
+    pass
